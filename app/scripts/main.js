633a351c3de9445020b6b323d2b7440873bde378
cj5 <stenny.cj@hotmail.com>
2016-04-01 15:33:23 -0500

    styles and js added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){    
+    function updateContainer() {
+        var theHeight = $(window).height();
+        var headerHeight = $('.logo').height();
+        var navTextHeight = $('nav a').height();
+        
+        $('header').css('height', headerHeight+20);
+        $('nav ul').css('margin-top', headerHeight/2+8 - navTextHeight/2);
+        $('nav ul').css('margin-bottom', headerHeight/2+8 - navTextHeight/2); 
+        console.log(headerHeight);         
+    }
+
+    updateContainer();
+
+    $(window).resize(function() {
+        updateContainer();
+    });
+});        
